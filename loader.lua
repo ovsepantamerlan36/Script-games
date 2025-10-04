e0aa6d5fd9fa841bd9ce963981392e79fc4968f4
TwizzleFuzz <ovsepantamerlan36@gmail.com>
2025-10-04 18:03:41 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,7 @@
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ovsepantamerlan36/Script-games/refs/heads/main/games-list.lua"))()
 
+local URL = Games[game.PlaceId]
+
+if URL then
+  loadstring(game:HttpGet(URL))()
+end
