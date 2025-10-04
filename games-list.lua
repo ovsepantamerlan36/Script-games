8325d4c08d9afca0c07ce2d330bf8eee7bc75088
TwizzleFuzz <ovsepantamerlan36@gmail.com>
2025-10-04 18:03:00 +0300

    Update games-list.lua

--- games-list.lua
+++ games-list.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [79205214213091] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Merge Tower Defense
+  [79205214213091] = "https://raw.githubusercontent.com/ovsepantamerlan36/Script-games/refs/heads/main/games/Merge%20Tower%20Defense.lua", -- Merge Tower Defense
 }
 
 return Games
